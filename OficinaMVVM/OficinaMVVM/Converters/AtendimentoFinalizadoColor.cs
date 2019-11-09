6951c8d11249d0ee28d5d3fbca5a647be73a20bd
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-11-09 10:02:11 -0300

    Aplicativo finalizado

---
+++ OficinaMVVM/OficinaMVVM/Converters/AtendimentoFinalizadoColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+
+
+namespace OficinaMVVM.Converters
+{
+    public class AtendimentoFinalizadoColor 
+    {
+        public  object Convert(object value , Type targetType, object  parameter,CultureInfo culture)
+        {
+            if ((bool)value)
+                return Color.Yellow;
+                 return Color.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
